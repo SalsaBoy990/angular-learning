ba383668d2d27a00717b71e0a83a2120dd1e743b
Gulácsi András <guland@protonmail.com>
2023-08-21 17:10:17 +0200

    add: Auth Guard to protect authenticated endpoints

---
+++ src/app/service/auth-guard.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import {CanActivate, Router} from "@angular/router";
+import {AuthService} from "./auth.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuardService implements CanActivate {
+
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+  ) { }
+
+  canActivate(): boolean {
+    if(!this.auth.currentUserValue) {
+      this.router.navigate(['login']).then(r => console.log(r));
+      return false;
+    } else {
+      return true;
+    }
+  }
+}
